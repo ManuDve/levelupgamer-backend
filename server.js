22eddbbf5f40e973cc2c83e558be96f8058826c3
Manuel Alfaro <m.alfaro@live.cl>
2025-12-01 22:43:59 -0300

    cors fix for public use

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
